c215fa9b2171be758c669c72a7c1f53eb3103c5c
serenty <serenty395@gmail.com>
2024-01-22 08:27:39 +0300

    hotfix change table  name

--- src/main/java/com/dark/online/repository/ProductRepository.java
+++ src/main/java/com/dark/online/repository/ProductRepository.java
@@ -11,5 +11,4 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductById(Long id);
-    List<Product> findAllProducts();
 }
