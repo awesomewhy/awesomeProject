9c80195dede8a2eddfb5460103685504c1665ff4
serenty <serenty395@gmail.com>
2024-01-31 03:10:36 +0300

    add some

--- src/main/java/com/dark/online/controller/user/UserController.java
+++ src/main/java/com/dark/online/controller/user/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/image")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class UserController {
