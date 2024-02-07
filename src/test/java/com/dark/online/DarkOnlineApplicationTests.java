5c0ede680a67badd8b0c24f94e559b23f3462545
ky <125137685+awesomewhy@users.noreply.github.com>
2024-02-07 14:05:39 +0300

    Update DarkOnlineApplicationTests.java

--- src/test/java/com/dark/online/DarkOnlineApplicationTests.java
+++ src/test/java/com/dark/online/DarkOnlineApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DarkOnlineApplicationTests {
+class OzonpplicationTests {
 
     @Test
     void contextLoads() {
